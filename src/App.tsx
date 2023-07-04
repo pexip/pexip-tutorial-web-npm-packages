ad79b092d200fbee75e07326fd99fa0fc4f11286
Marcos Cereijo <marcos.rodriguez@pexip.com>
2023-07-04 11:06:00 +0200

    Step.04-Exercise-Receive-presentation

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,7 @@ function App() {
   const [connectionState, setConnectionState] = useState<ConnectionState>(ConnectionState.Disconnected);
   const [localStream, setLocalStream] = useState<MediaStream | null>(null);
   const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
+  // TODO (01) Define state for presentationStream
   const [error, setError] = useState('');
 
   const [nodeDomain, setNodeDomain] = useState<string>('');
@@ -125,6 +126,7 @@ function App() {
 
   useEffect(() => {
     callSignals.onRemoteStream.add((stream) => setRemoteStream(stream));
+    // TODO (02) Listen to signal onRemotePresentationStream
     infinityClientSignals.onError.add((error) => {
       setConnectionState(ConnectionState.Error);
       setError(error.error);
@@ -159,6 +161,7 @@ function App() {
         <Conference
           localStream={localStream}
           remoteStream={remoteStream}
+          // TODO (03) Add the property presentationStream
           onAudioMute={handleAudioMute}
           onVideoMute={handleVideoMute}
           onDisconnect={handleDisconnect}
--- src/components/Conference/Conference.tsx
+++ src/components/Conference/Conference.tsx
@@ -23,19 +23,33 @@ const Video = React.memo((props: VideoProps) => {
 interface ConferenceProps {
   localStream: MediaStream | null;
   remoteStream: MediaStream | null;
+  // TODO (04) Add the property presentationSteam
   onAudioMute: (mute: boolean) => void;
   onVideoMute: (mute: boolean) => void;
   onDisconnect: () => void;
 }
 
 function Conference(props: ConferenceProps) {
+
+  // TODO (05) Define the state presentationInMain
+
+  // TODO (06) Create the function switchVideos
+  // TODO (07) Use the useCallback hook over the switchVideo function
+
+  // TODO (08) Define the additional classes for the conference component
+
+  // TODO (10) Use the useEffect hook to reset presentationInMain when the presentation is disabled
+
   return (
+    // TODO (09) Add additionalClasses to the className
     <div className='Conference'>
+      {/* TODO (11) Add onClick attribute to the video with memoizedSwitchVideos */}
       <Video className='remote-video'
          mediaStream={props.remoteStream}/>
       { props.localStream &&
         <Video className='local-video' mediaStream={props.localStream}/>
       }
+      {/* TODO (12) Add the Video HTML video for presentationStream */}
       <Toolbar
         className='toolbar'
         onAudioMute={props.onAudioMute}

6a7dd8215bb25f9bc8851f39149bce331d9669bb
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-08-21 13:18:13 +0200

    Step.03-Exercise-Mute-audio-and-video

--- src/components/Conference/Toolbar/Toolbar.tsx
+++ src/components/Conference/Toolbar/Toolbar.tsx
@@ -1,14 +1,25 @@
+// TODO (10) Import useState from react
 import { Button, Icon, IconTypes, Tooltip } from '@pexip/components'
 
 import './Toolbar.css'
 
 interface ToolbarProps {
   className: string
+  // TODO (11) Add onAudioMute prop
+  // TODO (12) Add onVideoMute prop
   onDisconnect: () => Promise<void>
 }
 
 export const Toolbar = (props: ToolbarProps): JSX.Element => {
   const className = [props.className, 'Toolbar'].join(' ')
+
+  // TODO (13) Add audioMuted and videoMuted states
+
+  // TODO (14) Add processingAudioMuted and processingVideoMuted states
+
+  // TODO (15) Add handleAudioMute function
+
+  // TODO (16) Add handleVideoMute function
 
   const handleHangUp = async (): Promise<void> => {
     await props.onDisconnect()
@@ -16,6 +27,8 @@ export const Toolbar = (props: ToolbarProps): JSX.Element => {
 
   return (
     <div className={className}>
+      {/* TODO (17) Add audio mute button */}
+      {/* TODO (18) Add video mute button */}
       <Tooltip text="Disconnect">
         <Button
           onClick={() => {

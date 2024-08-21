7608b11ce478bf33108f6132a605050460524bdf
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-08-21 11:37:00 +0200

    Step.01-Exercise-Join-a-conference

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+// TODO (01) Import all dependencies
 import './App.css'
 
+// TODO (02) Define infinityClientSignals
+// TODO (03) Define infinityClient
+
+// TODO (04) Define infinityClient
+
+// TODO (05) Define ConnectionState
+
 export const App = (): JSX.Element => {
-  return <div className="App"></div>
+  // TODO (06) Define the connectionState state
+
+  // TODO (07) Define the localAudioStream state
+  // TODO (08) Define the localVideoStream state
+  // TODO (09) Define the remoteStream state
+
+  // TODO (10) Define the error state
+
+  // TODO (11) Define the handleStartConference function
+
+  // TODO (12) Define the handleDisconnect function
+
+  // TODO (13) Define useEffect to initialize the infinityClient when the error state changes
+
+  // TODO (14) Define useEffect to initialize the events when the component is mounted
+
+  // TODO (15) Define a component depending on the connectionState
+  return <div className="App">{/* TODO (16) Render the component */}</div>
 }
--- src/components/Conference/Conference.tsx
+++ src/components/Conference/Conference.tsx
@@ -1,5 +1,8 @@
+// TODO (17) Import Video and Toolbar components
 import './Conference.css'
 
+// TODO (18) Define the Conference properties and pass them to the function
 export const Conference = (): JSX.Element => {
+  // TODO (19) Render the Video components for local and remote and the Toolbar component
   return <div className="Conference"></div>
 }

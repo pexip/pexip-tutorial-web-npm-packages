e7dc59011188d54e20bc69eb688f4a8fadc22cef
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-08-21 11:37:00 +0200

    Step.01-Exercise-Join-a-conference

--- src/components/Conference/Toolbar/Toolbar.tsx
+++ src/components/Conference/Toolbar/Toolbar.tsx
@@ -1,11 +1,19 @@
+// TODO (20) Import components from @pexip/components
 import './Toolbar.css'
 
 interface ToolbarProps {
   className: string
+  // TODO (21) Define onDisconnect property
 }
 
 export const Toolbar = (props: ToolbarProps): JSX.Element => {
   const className = [props.className, 'Toolbar'].join(' ')
 
-  return <div className={className}></div>
+  // TODO (22) Define the handleHangUp function
+
+  return (
+    <div className={className}>
+      {/* TODO (23) Add a new button to disconnect from the call */}
+    </div>
+  )
 }

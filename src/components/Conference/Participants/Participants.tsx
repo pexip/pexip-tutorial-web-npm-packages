e0d9594e4b1dc4a08dd823c61e45807fae844c31
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-08-22 15:18:50 +0200

    Step.08-Exercise-Participant-list

--- src/components/Conference/Participants/Participants.tsx
+++ src/components/Conference/Participants/Participants.tsx
@@ -1,7 +1,13 @@
+// TODO (13) Import the necessary components from the @pexip/components library
 import { Box } from '@pexip/components'
+// TODO (14) Import the Participant type from the @pexip/infinity library
 
 import './Participants.css'
 
 export const Participants = (): JSX.Element => {
-  return <Box className="Participants"></Box>
+  return (
+    <Box className="Participants">
+      {/* TODO (15) Add several elements to display the participant displayName and the state (isMuted and isCameraMuted) */}
+    </Box>
+  )
 }
